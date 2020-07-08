de8b98b01246e81f83251970b3110cd16aa3eb7d
Luis Sundar <sundarluis98@gmail.com>
2020-07-07 19:45:54 -0500

    js error display and fix with dependence -> lodash

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
+const _ = require('lodash')
 let data = [
     {
+        "id":1,
         "name":"joan"
     },
     {
+        "id":2,
         "name":"senix"
     },
     {
+        "id":3,
         "name":"daniel"
     },
 ]
+let copyArrayObjcts = _.cloneDeep(data);
+let copyArrayObjcts2 = data;
 let add = {
     temp : "xD"
 }
 data[0] = {...data[0],...add}
 
 console.log(data)
+console.log(copyArrayObjcts)
+console.log(copyArrayObjcts2)
