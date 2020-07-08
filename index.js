244d264a9b0393cc4dc490159659adfbf415cd81
Luis Sundar <sundarluis98@gmail.com>
2020-07-07 19:36:22 -0500

    starting

---
+++ index.js
@@ -0,0 +1,17 @@
+let data = [
+    {
+        "name":"joan"
+    },
+    {
+        "name":"senix"
+    },
+    {
+        "name":"daniel"
+    },
+]
+let add = {
+    temp : "xD"
+}
+data[0] = {...data[0],...add}
+
+console.log(data)
